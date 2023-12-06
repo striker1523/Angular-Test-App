733b037149aefacbd11e7fa44fb9d03116f6ef1d
Varisch <bartek0sobanski@gmail.com>
2023-12-06 09:33:51 +0100

    lab-b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RandomComponent } from './random/random.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
